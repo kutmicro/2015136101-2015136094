b51b09b5662f0d3cc54dd405a00448445469ff97
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-03 17:24:55 +0900

    Update SourceCode.cpp

--- SourceCode.cpp
+++ SourceCode.cpp
@@ -27,19 +27,21 @@ void loop() {
   int max_v = 0;
   pixels.clear();
   for(int i = 0;i < 7;i++){//입력받은 신호에 따라 네오픽셀을 제어
-    int l = eq.getLValue(i);
-    int r = eq.getRValue(i);
-    int v = (l<r? r:l);
-    max_v = (v>max_v? v:max_v);
-    for(int j = 0;j < 5;j++){
-      if(v > K_value*(5-j)){
-        on_pixel(i+(8*j));
+     //네오픽셀의 7번째 열까지 반복
+    int l = eq.getLValue(i); //i번째의 spectrumLvalues 값
+    int r = eq.getRValue(i); //i번째의 spectrumRvalues 값
+    int v = (l<r? r:l); // l과r중 큰갑을 v에 저장
+    max_v = (v>max_v? v:max_v); //v와 max_v중 큰값을 max_v에 저장
+    for(int j = 0;j < 5;j++){ //네오픽셀의 5번째 행까지 반복
+      if(v > K_value*(5-j)){ //v값이 K_value*(5-j)보다 크면
+        on_pixel(i+(8*j)); //color_map에 설정된 값으로 설정?????????????????????????
       }
     }
   }
+   //마지막 열을 표시한다 (5행 반복)
   for(int j = 0;j < 5;j++){//최대음량을 마지막 줄에 표시
-    if(max_v > K_value*(5-j)){
-      on_pixel(7+(8*j));
+    if(max_v > K_value*(5-j)){ //저장한 max_v값이 K_value*(5-j)값보다 크면
+      on_pixel(7+(8*j)); //color_map에 설정된 값으로 설정????????????????????????
     }
   }
   K_value = constrain(max_v/5,MIN_K_value,MAX_K_value);//5등분 분할의 기준을 음악의 최대값의 크기에 맞게 조절(변수 max_v/5의 값을 MIN_K_value~MAX_K_value 사이의 값으로 한정한다.)
