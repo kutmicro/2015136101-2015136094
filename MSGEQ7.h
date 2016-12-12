296f41f9739443ff73338ceb173ab628187825af
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-12-13 00:07:04 +0900

    Update MSGEQ7.h

--- MSGEQ7.h
+++ MSGEQ7.h
@@ -1,34 +1,24 @@
-// MSGEQ7.h
-// Library for controlling MSGEQ7 Audio Spectrum Analyzer Chip (https://www.sparkfun.com/products/10468)
-// Credit to J Skoba (http://nuewire.com/info-archive/msgeq7-by-j-skoba/) for the original implementation
-// retouch by Yevgeny
-// thanks to J Skoba , justinb26! 
-
-
 #if defined(ARDUINO) && ARDUINO >= 100
-  #include "Arduino.h"
+#include "Arduino.h"
 #else
-  #include "WProgram.h"
-  #include <thePins_arduino.h>
+#include "WProgram.h"
+#include <thePins_arduino.h>
 #endif
 
 class MSGEQ7
 {
-	public:
-		MSGEQ7();
-		void init(int left, int right, int strobePin, int resetPin);
-		void init();
-		void poll();
+public:
+	MSGEQ7();
+	void init(int value, int strobePin, int resetPin);
+	void init();
+	void poll();
 
-		int getLValue(int index);
-		int getRValue(int index);
+	int getValue(int index);
 
-	private:
-		int L_pin; // read left value
-		int R_pin; // read right value
-		int strobePin; // strobe is attached to digital pin 2
-		int resetPin; // reset is attached to digital pin 3
+private:
+	int Pin; // read left value
+	int strobePin; // strobe is attached to digital pin 2
+	int resetPin; // reset is attached to digital pin 3
 
-		int spectrumLValues[7]; 
-		int spectrumRValues[7];
+	int spectrumValues[7];
 };
